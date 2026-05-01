3197fdfe51fd6c7c13589dd00b96fad8f40b2696
Dmitry Borodin <git@DmitryBorodin.com>
2026-05-01 11:41:51 +0200

    Prepare F-Droid metadata release v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.cloudbasepredictor"
         minSdk = 25
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "com.cloudbasepredictor.HiltTestRunner"
 
